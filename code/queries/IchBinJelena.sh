db379bbd40bdd5dad7ce7d0d80e6ce9c5a2a0f3e
Jana <jana.lasser@ds.mpg.de>
2021-06-15 09:59:34 +0200

    added #IchBinJelena, #IchBinMelek and #IchBinNichtHanna to the Twitter API queries

---
+++ code/queries/IchBinJelena.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+twarc2 search --start-time 2018-01-01 --end-time 2021-06-13 --archive '(#ichbinjelena OR #IchBinJelena OR #ichbinJelena OR #ichBinjelena OR #Ichbinjelena OR #ICHBINJELENA)' > ../data/IchBinJelena.jsonl

f7389b6dadbde0d3899537be9145a5c93bf67eb1
trutzig89182 <>
2021-06-16 21:22:02 +0200

    Script language code frquencies
    Script for collecting language code frequencies from one or multiple jsonl.xz files. The requencies are written into a csv file.
    I put some first results into ./tables

---
+++ code/language_frequencies/README.md
@@ -0,0 +1,40 @@
+# Analysis of Tweets for the #IchBinHannah Campaign
+
+Autor_innen: Jana Lasser, Stefan Laser, Migle Bareikyte, Miria Blöchel, Tim Schatto-Eckrodt, Elen Le Foll, Christian Funk, David Adler
+
+[![CC BY 4.0][cc-by-shield]][cc-by]
+
+## Script for language codes frequencies
+
+The script collects language codes from jsonl.xz files or foldes containing multiple such files. Its returns a .csv file with absolute and relative frequencies and saves it within a subfolder „lf_results" of the folder you run the script in. The files are automatically decompressed. Retweets are exclude from the frequency count.
+
+### Usage
+
+Navigate to the folder with the script.
+
+```python3 language_tweets.py [ARGUMENTS] PATH```
+
+ARGUMENTS: 
+
+`file` if you want to analyse a single file
+
+`folder` if you want to analyse a 
+
+`--help` for basic information
+
+PATH: either (relative or absolute) path to file or to folder
+
+### Todos
+Sorting csv lines by frequency
+
+
+
+
+This work is licensed under a
+[Creative Commons Attribution 4.0 International License][cc-by].
+
+[![CC BY 4.0][cc-by-image]][cc-by]
+
+[cc-by]: http://creativecommons.org/licenses/by/4.0/
+[cc-by-image]: https://i.creativecommons.org/l/by/4.0/88x31.png
+[cc-by-shield]: https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg
---
+++ code/language_frequencies/language_tweets.py
@@ -0,0 +1,103 @@
+import json
+import os
+import jsonlines
+import lzma
+from collections import Counter
+import sys
+from datetime import datetime
+import csv
+
+directory = os.getcwd()
+# x = input("Enter File (F) or Directory (D)")
+
+def main(arguments):
+    languageArray = []
+    if arguments[1]=="file":
+        path = arguments[2]
+        languageArray = tweet_languages_in_file(path)
+    elif arguments[1]=="folder":
+        path = arguments[2]
+        languageArray = tweet_languages_in_folder(path)
+    elif arguments[1]=="-h":
+        print("Form: python3 language_tweets.py [Arg1] [path to file or folder]\n Args1:\n folder -> alle Datein im Ordner\n file -> alle Tweets in einer Datei")
+        quit()
+    language_count(languageArray, path)
+    quit()
+
+
+
+def tweet_languages_in_file(filepath):
+    path = filepath
+    #print("path: " + path)
+    languageArray = []
+    if path.endswith(".jsonl.xz"):
+        with lzma.open(path, "rb") as jsonlentry:
+            # opens file with jsonlines
+            reader = jsonlines.Reader(jsonlentry)
+            for obj in reader:
+                for entry in obj["data"]:
+                    if not "referenced_tweets" in entry or entry["referenced_tweets"][0]["type"]!="retweeted":
+                        #if not eintrag["type"]=="retweeted":
+                        if "lang" in entry:
+                            #print(obj.data.entity["lang"])
+                            #  if not obj["lang"]=="":
+                            languageArray.append(entry["lang"])
+                        else:
+                            print("lang does not exist")
+    return(languageArray)
+
+
+def tweet_languages_in_folder(folderpath):
+    path = folderpath
+    print("path:" + path)
+    languageArray = []
+    for entry in os.scandir(path):
+        # selects all files that end with .jsonl.gz
+        if entry.path.endswith(".jsonl.xz") and entry.is_file():
+            # unzips file so we can work with it
+            with lzma.open(entry, "rb") as jsonlentry:
+                # opens file with jsonlines
+                reader = jsonlines.Reader(jsonlentry)
+                for obj in reader:
+                    for entry in obj["data"]:
+                        if not "referenced_tweets" in entry or entry["referenced_tweets"][0]["type"]!="retweeted":
+                            #if not eintrag["type"]=="retweeted":
+                            if "lang" in entry:
+                                #print(obj.data.entity["lang"])
+                                #  if not obj["lang"]=="":
+                                languageArray.append(entry["lang"])
+                            else:
+                                print("lang does not exist")
+    return(languageArray)
+
+def language_count(languageArray, filepath):
+    # sums all language codes
+    sumcounts = len(languageArray)
+    # creates a counter with absolute frequencies
+    counts = Counter(languageArray)
+    #timestamp to make filenames unique
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    #checks if folder for results exists, if not creates it
+    if not os.path.exists("lf_results"):
+        os.makedirs("lf_results")
+    csvfile = directory + f"/lf_results/lang-freq_{timestamp}.csv"
+    sourcename = filepath
+    with open(csvfile, "w") as file:
+      fieldnames = ["Country", "Frequency"]
+      writer = csv.writer(file)
+      writer.writerow(["Language frequences for file or folder: ", sourcename, "%"])
+      writer.writerow(fieldnames)
+      for key, value in counts.items():
+        writer.writerow([key, value, int(value/sumcounts*100)])
+    # try:
+    #     with open(csvfile, "w") as file:
+    #       fieldnames = ["Country", "Frequency"]
+    #       writer = csv.writer(file)
+    #       writer.writerow(["Language frequences for file or folder: ", sourcename])
+    #       writer.writerow(fieldnames)
+    #       writer.writerows(counts.items() + "," + float(counts.value()/sumcounts))
+    # except:
+    #       print("Could not build csv-file.")
+
+if __name__ == '__main__':
+    main(sys.argv)

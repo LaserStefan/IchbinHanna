c65f3ac27071c909f1478aa537b8475ed784e184
LaserSteff <85789220+LaserSteff@users.noreply.github.com>
2021-06-17 13:35:44 +0200

    Piechart for lang

---
+++ code/piechart.py
@@ -0,0 +1,12 @@
+labels = 'German', 'English', 'Others (all below < 1%)'
+sizes = [83, 11, 6]
+explode = (0.1, 0, 0)  # only "explode" the 1ST slice
+
+fig1, ax1 = plt.subplots()
+
+ax1.pie(sizes, explode=explode, labels=labels, colors= ['#756bb1', '#bcbddc', '#efedf5'], autopct='%1.0f%%',
+        shadow=False, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.set_title("Language of all #IchbinHanna Tweets")
+plt.savefig('../pie_users.png', dpi=300)
+plt.show()

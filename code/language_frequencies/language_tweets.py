88f318198f6180915ec837708b7bad9b1daffb8c
trutzig89182 <adler.david@freenet.de>
2021-06-18 13:12:04 +0200

    Update language_tweets.py
    cleaned code

--- code/language_frequencies/language_tweets.py
+++ code/language_frequencies/language_tweets.py
@@ -12,69 +12,65 @@ directory = os.getcwd()
 def main(arguments):
     languageArray = []
     if arguments[1]=="file":
-        path = arguments[2]
+        sourcepath = arguments[2]
         # calls tweet_languages_in_file for the file-path given when starting the script, gets array of language codes
-        languageArray = tweet_languages_in_file(path)
+        languageArray = tweet_languages_in_file(sourcepath)
     elif arguments[1]=="folder":
-        path = arguments[2]
+        sourcepath = arguments[2]
         # calls tweet_languages_in_folder for the folder-path given when starting the script,  gets array of language codes
-        languageArray = tweet_languages_in_folder(path)
+        languageArray = tweet_languages_in_folder(sourcepath)
     elif arguments[1]=="-h" or arguments[1]=="--help":
-        print("Syntax: python3 language_tweets.py [ARGUMENTS] [PATH TO FILE OR FOLDER]\n ARGUMENTS:\n folder -> gets language count from all .json.xs files of the given folder\n file -> gets language count from a specific .jsonl.xs file")
+        print("Syntax: python3 language_tweets.py [ARGUMENTS] [PATH TO FILE OR FOLDER] \nARGUMENTS:\n folder -> gets language count from all .json.xs files of the given folder\n file -> gets language count from a specific .jsonl.xs file\n --help -> gives syntax")
         quit()
     else:
         print("You have entered arguments, that make no sense to me")
         quit()
-    language_count(languageArray, path)
+    language_count(languageArray, sourcepath)
     quit()
 
-
-
+######## FUNCTIONS #########
 def tweet_languages_in_file(filepath):
-    path = filepath
-    #print("path: " + path)
+    entry = filepath
+    print(f"Language frequencies for file: {entry}")
     languageArray = []
-    if path.endswith(".jsonl.xz"):
-        with lzma.open(path, "rb") as jsonlentry:
-            # opens file with jsonlines
-            reader = jsonlines.Reader(jsonlentry)
-            for obj in reader:
-                for entry in obj["data"]:
-                    if not "referenced_tweets" in entry or entry["referenced_tweets"][0]["type"]!="retweeted":
-                        #if not eintrag["type"]=="retweeted":
-                        if "lang" in entry:
-                            #print(obj.data.entity["lang"])
-                            #  if not obj["lang"]=="":
-                            languageArray.append(entry["lang"])
-                        else:
-                            print("lang does not exist")
+    if entry.endswith(".jsonl.xz"):
+        get_lang_from_entry(entry, languageArray)
     return(languageArray)
 
 
 def tweet_languages_in_folder(folderpath):
-    path = folderpath
-    print("path:" + path)
+    folderpath=folderpath
+    print(f"Language frequencies for folder: {folderpath}")
     languageArray = []
-    for entry in os.scandir(path):
+    for entry in os.scandir(folderpath):
         # selects all files that end with .jsonl.gz
         if entry.path.endswith(".jsonl.xz") and entry.is_file():
-            # unzips file so we can work with it
-            with lzma.open(entry, "rb") as jsonlentry:
-                # opens file with jsonlines
-                reader = jsonlines.Reader(jsonlentry)
-                for obj in reader:
-                    for entry in obj["data"]:
-                        if not "referenced_tweets" in entry or entry["referenced_tweets"][0]["type"]!="retweeted":
-                            #if not eintrag["type"]=="retweeted":
-                            if "lang" in entry:
-                                #print(obj.data.entity["lang"])
-                                #  if not obj["lang"]=="":
-                                languageArray.append(entry["lang"])
-                            else:
-                                print("lang does not exist")
+            get_lang_from_entry(entry, languageArray)
     return(languageArray)
 
-def language_count(languageArray, filepath):
+
+def get_lang_from_entry(entry, languageArray):
+    # unzips file so we can work with it
+    number_of_retweets = 0
+    no_lang = 0
+    with lzma.open(entry, "rb") as jsonlentry:
+        # opens file with jsonlines
+        reader = jsonlines.Reader(jsonlentry)
+        for obj in reader:
+            for entry in obj["data"]:
+                if not "referenced_tweets" in entry or entry["referenced_tweets"][0]["type"]!="retweeted":
+                    if "lang" in entry:
+                        languageArray.append(entry["lang"])
+                    else:
+                        print("lang does not exist")
+                        no_lang += 1
+                else:
+                    number_of_retweets += 1
+    return(languageArray)
+
+
+def language_count(languageArray, sourcepath):
+    sourcename = sourcepath
     # gives the amount of all language codes
     sumcounts = len(languageArray)
     # creates a counter with absolute frequencies
@@ -85,8 +81,6 @@ def language_count(languageArray, filepath):
     if not os.path.exists("lf_results"):
         os.makedirs("lf_results")
     csvfile = directory + f"/lf_results/lang-freq_{timestamp}.csv"
-    sourcename = filepath
-    und_count = 0
     # writes counts dictionary into csv file
     try:
         with open(csvfile, "w") as file:
@@ -96,7 +90,7 @@ def language_count(languageArray, filepath):
           writer.writerow(fieldnames)
           for key, value in counts.items():
             relative_frequency = value/sumcounts*100
-            writer.writerow([key, value, round(relative_frequency, 2)])      
+            writer.writerow([key, value, round(relative_frequency, 2)])
     except:
         print("Could not build csv-file.")
 

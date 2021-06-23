0655677c2dd681d7b8fe8057d617708c4e6d4afe
LaserSteff <85789220+LaserSteff@users.noreply.github.com>
2021-06-23 09:39:14 +0200

    Updates

---
+++ code/queries/Updates/FristIstFrust.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+twarc2 search --start-time 2021-06-15 --end-time 2021-06-21 --archive '(#FristIstFrust OR #frististfrust OR #Frististfrust OR #fristIstfrust OR #frististFrust OR #FristIstfrust OR #FrististFrust OR #fristIstFrust)' > /media/s/Linux_storage/Analyse_Verkehrswende_Transformation/Data/Hanna_Update.jsonl
---
+++ code/queries/Updates/Hanna.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+twarc2 search --start-time 2021-06-15 --end-time 2021-06-21 --archive '(#ichbinhannah OR #IchBinHannah OR #ichbinHannah OR #ichBinhannah OR #Ichbinhannah OR #ichBinHannah OR IchbinHannah OR IchBinhannah OR #ICHBINHANNAH OR #ichbinhanna OR #IchBinHanna OR #ichbinHanna OR #ichBinhanna OR #Ichbinhanna OR #ichBinHanna OR IchbinHanna OR IchBinhanna OR #ICHBINHANNA)' > /media/s/Linux_storage/Analyse_Verkehrswende_Transformation/Data/Hanna_Update.jsonl
